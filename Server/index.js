03e23d33f626c79ae60552e5bd56da3ab480d29f
codewidhypno <bibinbabuman@gmail.com>
2021-02-12 22:54:46 +0530

    Authentication Module

--- Server/index.js
+++ Server/index.js
@@ -3,8 +3,10 @@ const mongoose =  require('mongoose')
 const bodyParser = require('body-parser')
 
 const app = express()
+ 
+// const url = 'mongodb+srv://root:RdRXotePjJwKcv6u@cluster0.eyhy3.mongodb.net/thrift_store?retryWrites=true&w=majority'
 
-const url = 'mongodb+srv://root:RdRXotePjJwKcv6u@cluster0.eyhy3.mongodb.net/thrift_store?retryWrites=true&w=majority'
+const url = 'mongodb://localhost/thrift_store'
 
 mongoose.connect(url, {useNewUrlParser:true,useUnifiedTopology: true, useFindAndModify: false})
 const con = mongoose.connection
@@ -25,6 +27,12 @@ app.get('/', (req, res) => {
     });
 });
 
+
+// routes
+
+require('./app/Routes/category.routes')(app)
+require('./app/Routes/user.routes')(app)
+require('./app/Routes/Admin/user.routes')(app)
 
 app.listen(9000, () => {
     console.log('Server started')
